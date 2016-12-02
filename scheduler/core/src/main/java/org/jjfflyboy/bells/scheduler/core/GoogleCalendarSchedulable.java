6a21d7d907eafe0d8a4856fda126ca1a17c31140
John J. Franey <jjfraney@gmail.com>
2016-12-01 20:41:34 -0500

    support event parsing to offer more than call-to-mass

--- scheduler/core/src/main/java/org/jjfflyboy/bells/scheduler/core/GoogleCalendarSchedulable.java
+++ scheduler/core/src/main/java/org/jjfflyboy/bells/scheduler/core/GoogleCalendarSchedulable.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -29,12 +30,56 @@ public class GoogleCalendarSchedulable extends AbstractPeriodicSchedulable {
         List<Calendar.Event> events = calendar.getEvents();
         events.stream()
                 .forEach(e -> LOGGER.debug("event={}, time={}", e.getTitle(), e.getTime().toLocalDateTime()));
-        LOGGER.debug("calendar received");
-        List<Scheduler.OneShotSchedulable> callToMass = events.stream()
-                .filter(e -> "mass".equals(e.getTitle()))
-                .map(e -> new PlaySongSchedulable("call-to-mass.ogg", e.getTime().minusMinutes(2).toLocalDateTime()))
+        LOGGER.debug("calendar received.  event count={}", events.size());
+        List<Scheduler.OneShotSchedulable> schedulables = events.stream()
+                .map(SongEvent::new)
+                .map(e -> new PlaySongSchedulable(e.getTitle(), e.getTime().toLocalDateTime()))
                 .collect(Collectors.toList());
-        return callToMass;
+        return schedulables;
+    }
+
+    private class SongEvent implements Calendar.Event {
+        private final Calendar.Event event;
+        public SongEvent(Calendar.Event event) {
+            this.event = event;
+        }
+
+        @Override
+        public ZonedDateTime getTime() {
+            ZonedDateTime result;
+            if(isMass()) {
+                result = event.getTime().minusMinutes(2);
+            } else {
+                result = event.getTime();
+            }
+            return result;
+        }
+
+        /**
+         * reads the delegate's title and creates a song title.
+         * @return
+         */
+        @Override
+        public String getTitle() {
+            String result = null;
+            if(isMass()) {
+                result = "call-to-mass.ogg";
+            } else if(isPlay()) {
+                result = parseSongTitle();
+            }
+            return result;
+        }
+
+        private String parseSongTitle() {
+            return event.getTitle().replaceFirst("play", "").trim();
+        }
+        private boolean isPlay() {
+            return event.getTitle().startsWith("play");
+        }
+
+        private boolean isMass() {
+            return event.getTitle().toLowerCase().startsWith("mass");
+        }
     }
     @Override
     public Callable<Void> getCallable() {
