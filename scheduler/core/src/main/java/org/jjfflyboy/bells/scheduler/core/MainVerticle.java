553e7f40052152a31db1fd77ad4ef92161611f11
John J. Franey <jjfraney@gmail.com>
2017-03-11 11:58:18 -0500

    Refactor: replace example's convenient Runner class with builtin vertx Launcher

--- scheduler/core/src/main/java/org/jjfflyboy/bells/scheduler/core/MainVerticle.java
+++ scheduler/core/src/main/java/org/jjfflyboy/bells/scheduler/core/MainVerticle.java
@@ -3,15 +3,13 @@ package org.jjfflyboy.bells.scheduler.core;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Launcher;
 import io.vertx.core.json.Json;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
 /**
- * An example illustrating how worker verticles can be deployed and how to interact with them.
- * <p>
- * This example prints the name of the current thread at various locations to exhibit the event loop <-> worker
- * thread switches.
+ * @author jfraney
  */
 public class MainVerticle extends AbstractVerticle {
 
@@ -19,19 +17,27 @@ public class MainVerticle extends AbstractVerticle {
 
     // Convenience method so you can run it in your IDE
     public static void main(String[] args) {
+        if(args.length == 0) {
+            args = new String[]{"run", MainVerticle.class.getName()};
+        }
+        new Launcher().dispatch(args);
+    }
 
+    static {
+        // finds extension for jdk8 dates, ...
         Json.mapper.findAndRegisterModules();
+
+        // big decimal retains precision
         Json.mapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
-        Runner.runExample(MainVerticle.class);
     }
 
     @Override
     public void start() throws Exception {
         DeploymentOptions asWorker = new DeploymentOptions().setWorker(true);
-        vertx.deployVerticle("org.jjfflyboy.bells.scheduler.core.GoogleCalendarVerticle", asWorker);
-        vertx.deployVerticle("org.jjfflyboy.bells.scheduler.core.PlayVerticle", asWorker);
+        vertx.deployVerticle(GoogleCalendarVerticle.class.getName(), asWorker);
+        vertx.deployVerticle(PlayVerticle.class.getName(), asWorker);
 
-        vertx.deployVerticle("org.jjfflyboy.bells.scheduler.core.SchedulerVerticle");
+        vertx.deployVerticle(SchedulerVerticle.class.getName());
 
     }
 }
